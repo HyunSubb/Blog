941430da3804769bf2bf3e30e3f4d56448ac8e52
신현섭 <didakd22@naver.com>
2025-07-15 04:08:33 +0900

    ControllerAdvice 추가

--- src/main/java/com/hyunsub/Blog/request/PostCreate.java
+++ src/main/java/com/hyunsub/Blog/request/PostCreate.java
@@ -3,6 +3,7 @@ package com.hyunsub.Blog.request;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
